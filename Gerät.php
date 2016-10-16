9f981594d1716c27d463f21054c6ee2270c07aeb
Dave Thomas <dave.thomas@trader.ca>
2016-10-16 15:26:05 -0700

    Who knows...

---
+++ Gerät.php
@@ -0,0 +1,8 @@
+<?php
+echo "loading Gerät<br>";
+class Gerät{
+    function Gerät(){
+        $this->name = "testgerät";
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include "Gerät.php";
+
+echo "My first PHP script!<br>";
+
+
+$test = new Gerät();
+
+echo $test->name;
+?>
